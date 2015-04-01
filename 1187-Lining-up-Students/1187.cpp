befd7d74cb1c7dd2e6a91dbd8005890b8abc9a60
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-04-02 00:17:34 +0600

    Create 1187.cpp

---
+++ 1187-Lining-up-Students/1187.cpp
@@ -0,0 +1,81 @@
+//!
+///
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+ ** @Category:
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("E:/a.txt", "r", stdin);
+#define write freopen("E:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll long long
+#define mod 1000000007
+#define mx 100010
+#define gc getchar
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};
+int gcd(int a, int b) {
+    return b?gcd(b,a%b):a;
+}
+
+int main() {
+	#ifdef LOCAL
+    open
+    #endif // LOCAL
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        out(C);
+        int n;
+        read(n);
+        int a;read(a);
+        int ans=1;
+        for(int i=2;i<=n;i++){
+            read(a);
+            if(i-a<=ans)ans++;
+        }
+        printf("%d\n",ans);
+    }
+    return 0;
+}
+
+
+
+
+
