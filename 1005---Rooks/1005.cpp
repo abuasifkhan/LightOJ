b27fb47ce264217a699cf96695a8e0b01ed859c9
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-06-13 12:33:04 +0600

    Create 1005.cpp

---
+++ 1005---Rooks/1005.cpp
@@ -0,0 +1,89 @@
+//! Bismillahi-Rahamanirahim.
+///
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+ ** @Category:
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll unsigned long long
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};
+
+ll dp[35][35][910], Rw, Cl, K;
+
+ll gen(ll r, ll c, ll k){
+    if(k>r or k>c)return 0;
+    if(k==0)return 1;
+
+    ll &ret=dp[r][c][k];
+    if(ret!=-1)return ret;
+    ret = gen(r,c-1,k);
+    ret += r*gen(r-1,c-1,k-1);
+    return ret;
+}
+
+int main() {
+	#ifdef LOCAL
+    open
+    #endif // LOCAL
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        CLEAR(dp,-1);
+        read(Rw);read(K);
+        out(C);
+        ll ans=gen(Rw,Rw,K);
+        printf("%llu\n",ans);
+    }
+    return 0;
+}
+
+
+
+
+
