835a526ffea71681fe1bc55efa0544c2a0ecd015
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-06-13 16:43:47 +0600

    Create 1027.cpp

---
+++ 1027-A-Dangerous-Maze/1027.cpp
@@ -0,0 +1,95 @@
+//! Bismillahi-Rahamanirahim.
+///
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+ ** @Category:
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll long long
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};
+
+int main() {
+	#ifdef LOCAL
+    open
+    #endif // LOCAL
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        out(C);
+        int n;
+        read(n);
+        int leftu=n,rightu=0,rightd=n, leftd=n;
+
+        for(int i=0;i<n;i++){
+            int a;read(a);
+            if(a<0){
+                leftu-=1;
+            }
+            rightu+=abs(a);
+        }
+        int l=__gcd(leftu,leftd);
+        int r=__gcd(rightu,rightd);
+        leftu/=l;leftd/=l; rightd/=r;rightu/=r;
+//        cout<<leftu<<" "<<leftd<<" "<<rightu<<" "<<rightd<<endl;
+        if(leftu==0){
+            printf("inf\n");continue;
+        }
+        rightu*=leftd;
+        rightd*=leftu;
+        r=__gcd(rightu,rightd);rightu/=r;rightd/=r;
+        if(rightd==0)printf("inf\n");
+        printf("%d/%d\n",rightu,rightd);
+    }
+    return 0;
+}
+
+
+
+
+
