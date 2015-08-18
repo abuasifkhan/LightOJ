78ca290fb9d44f28b2634bd78b44e1915edbb276
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-08-18 15:25:43 +0600

    Create 1179.cpp

---
+++ 1179---Josephus-Problem/1179.cpp
@@ -0,0 +1,100 @@
+//! Bismillahi-Rahamanirahim.
+///
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll long long
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+/*
+    Let, f(n,k) be the position of the survivor.
+    He cannot survive if we put him a*k positions (a>=0).
+    We have to shift the survivors position to
+     next_position+k. So the next position will be
+     f(n,k) = f(n-1,k)+k.
+
+    As the circle size is decreasing we have to modularize
+    the survivors position, so f(n,k)=(f(n-1,k)+k)%n
+
+*/
+
+int f(int n, int k){
+    if(n==0)return 1;
+    return (f(n-1,k)+k)%n;
+}
+
+int main() {
+	#ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        out(C);
+        int n,k;read(n);read(k);
+        printf("%d\n",f(n,k)+1);
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
+
