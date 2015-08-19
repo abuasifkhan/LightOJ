50815dcc0cea53364b44ffd21526628f9970c489
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-08-19 12:50:39 +0600

    Create 1356.cpp

---
+++ 1356-Prime-Independence/1356.cpp
@@ -0,0 +1,221 @@
+//! Bismillahi-Rahamanirahim.
+///
+
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pi 3.141592653589793
+#define pii pair <int, int>
+#define pb push_back
+#define pf printf
+#define S(x) scanf("%d", &x)
+#define SL(x) scanf("%ld", &x)
+#define out(C) printf("Case %d: ", C);
+#define FOR(i, x, y) for(int i=x; i<=y; i++)
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define MAX (1+(1<<5))
+#define inf 0x7fffffff
+#define ll long long
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+struct Edge {
+    int from, to, cap, flow, index;
+    Edge(int from, int to, int cap, int flow, int index):
+        from(from),to(to),cap(cap),flow(flow),index(index){}
+};
+
+struct Dinic {
+    int N;
+    vector< vector<Edge> >G;
+    vector<Edge * >dad;
+    vector<int>Q;
+
+    Dinic(int N):N(N), G(N), dad(N), Q(N){}
+
+    void AddEdge(int from, int to ,int cap){
+        G[from].push_back(Edge(from, to, cap, 0, G[to].size()));
+        if(from==to)G[from].back().index++;
+        G[to].push_back(Edge(to,from, 0,0, G[from].size()-1));
+    }
+
+    long long BlockingFlow(int s, int t){
+        fill(dad.begin(),dad.end(),(Edge *)NULL);
+        dad[s] = &G[0][0]-1;
+
+        int head=0, tail=0;
+        Q[tail++]=s;
+        while(head<tail){
+            int x = Q[head++];
+            for(int i=0;i<G[x].size(); i++){
+                Edge &e = G[x][i];
+                if(!dad[e.to] and e.cap-e.flow>0){
+                    dad[e.to]=&G[x][i];
+                    Q[tail++]=e.to;
+                }
+            }
+        }
+        if(!dad[t])return 0;
+
+        long long totflow=0;
+        for(int i=0;i<G[t].size(); i++){
+            Edge *start=&G[G[t][i].to][G[t][i].index];
+            int amt=inf;
+            for(Edge*e = start; amt&&e!=dad[s]; e=dad[e->from]){
+                if(!e){
+                    amt=0;break;
+                }
+                amt=min(amt, e->cap - e->flow);
+            }
+            if(amt==0)continue;
+            for(Edge *e=start; amt && e!=dad[s]; e=dad[e->from]){
+                e->flow += amt;
+                G[e->to][e->index].flow-=amt;
+            }
+            totflow+=amt;
+        }
+        return totflow;
+    }
+
+    long long GetMaxFlow(int s, int t){
+        long long totflow=0;
+        while(long long flow=BlockingFlow(s,t))
+            totflow+=flow;
+        return totflow;
+    }
+};
+
+bitset<1000010>p;
+vector<int>primes;
+int factors[1000010];
+
+void sieve(int sz=1000000){
+    p.set();
+    p[1]=0;
+    for(ll i=2;i<=sz;i++){
+        if(p[i]){
+            primes.pb(i);
+            for(ll j=i*i;j<=sz;j+=i)
+                p[j]=0;
+        }
+    }
+}
+
+void preproc(){
+    sieve();
+    for(int i=2;i<=500000;i++){
+        ll t=i,cnt=0;
+        for(int j=0;(ll)primes[j]*primes[j]<=t;j++){
+            while(t%primes[j]==0){
+                cnt++;
+                t/=primes[j];
+            }
+        }
+        if(t!=1)
+            cnt++;
+        factors[i]=(cnt%2);
+    }
+}
+
+int inp[500010],arr[40010], n,Max;
+
+int solve(){
+    int source=2*n+2;
+    int sink=source+1;
+    Dinic flow(sink+2);
+    for(int i=1;i<=n;i++){
+        int u=arr[i];
+        for(int j=0;j<primes.size();j++){
+            if(u*primes[j]>Max)break;
+            int v=u*primes[j];
+
+            if(inp[v]!=-1){
+                v=inp[v];
+                if(factors[u]==0){
+                    flow.AddEdge(i,n+v,100);
+                }
+                else{
+                    flow.AddEdge(v,n+i,100);
+                }
+            }
+        }
+    }
+
+
+    for(int i=1;i<=n;i++){
+        flow.AddEdge(source,i,1);
+        flow.AddEdge(i+n,sink,1);
+    }
+
+    return flow.GetMaxFlow(source,sink);
+}
+
+int main() {
+	#ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+    preproc();
+    int test;
+    read(test);
+    FOR(C, 1, test) {
+        out(C);
+        read(n);
+        CLEAR(inp,0);
+        Max=0;
+        for(int i=1;i<=n;i++){
+            read(arr[i]);
+            Max=max(Max,arr[i]);
+            inp[arr[i]]=i;
+        }
+        printf("%d\n",n-solve());
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
+
