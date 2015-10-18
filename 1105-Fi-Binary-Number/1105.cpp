e13f3f03c79f5f521b19343e5ded86733c051d35
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2015-10-18 20:34:36 +0600

    Create 1105.cpp

---
+++ 1105-Fi-Binary-Number/1105.cpp
@@ -0,0 +1,102 @@
+//! Bismillahi-Rahamanirahim.
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pii pair <int, int>
+#define pb push_back
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define inf (1<<28)
+#define ll long long
+#define mod 1000000007
+#define gc getchar
+#define debug_array(a,n) for(int i=0;i<n;i++) cout<<a[i].in<<" "; cout<<endl;
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define fs first
+#define sc second
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+ll fib[101];
+
+int main() {
+    #ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+    // Can you solve it by brute force?
+    // LDC
+    // Are you sure you want to code so soon?? -_-
+    // Simplify your code a little more. :)
+
+    fib[0]=1;
+    fib[1]=2;
+    int idx=1;
+    while(fib[idx]<=2000000000){
+        idx++;
+        fib[idx]=fib[idx-1]+fib[idx-2];
+    }
+
+    int test;
+    read(test);
+    for(int C=1;C<=test;C++) {
+        printf("Case %d: ", C);
+        int n;read(n);
+        int I=0;
+        while(fib[I+1]<=n)I++;
+        string ans;
+        ans.pb('1');
+        int sum=fib[I];
+        while(I--){
+            if(sum+fib[I]<=n){
+                ans.pb('1');
+                sum+=fib[I];
+            }
+            else ans.pb('0');
+        }
+        cout<<ans<<endl;
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
