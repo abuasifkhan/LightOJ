49f0a99d28130c4f8b13b3850a488846e51f43f0
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2016-01-16 21:41:49 +0600

    Create 1098.cpp

---
+++ 1098-A-New-Function/1098.cpp
@@ -0,0 +1,90 @@
+//! Bismillahi-Rahamanirahim.
+/** ========================================**
+ ** @Author: A Asif Khan Chowdhury
+/** ========================================**/
+
+
+/**................ Headers ................**/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**................ Macros ................**/
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pii pair <int, int>
+#define pb push_back
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define inf (1<<28)
+#define ll unsigned long long
+#define mod 1000000007
+#define debug cout<<"ok"<<endl;
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define fs first
+#define sc second
+#define mx 100010
+
+//Fast Reader
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while(~c&&c<'0'||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+// int X[]= {-1, 0, 1, 0};   //x 4 direction
+// int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+
+ll CSOD(ll n){
+    ll ret=0ull;
+    for(ll i=2;i*i<=n;i++){
+        ll j = n/i;
+        ret+= j*(j+1)/2 - i*(i-1)/2;
+        ret+= i*(j-i);
+    }
+    return ret;
+}
+
+ll n;
+
+int main() {
+    #ifdef LOCAL
+    open
+    double st=clock(),en;
+    #endif // LOCAL
+
+    int test;
+    read(test);
+    for(int C=1;C<=test;C++) {
+        printf("Case %d: ", C);
+        read(n);
+        ll ans = CSOD(n);
+        printf("%llu\n",ans);
+    }
+
+    #ifdef LOCAL
+    en=clock();
+    cout<<"Time: ";
+    cerr<<(double)(en-st)/CLOCKS_PER_SEC<<endl;
+    #endif // LOCAL
+    return 0;
+}
+
+
+
+
