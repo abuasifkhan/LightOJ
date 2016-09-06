2ea3028dacaedce67bb53d2499c1b05970ea19b8
Abu Asif Khan Chowdhury <abuasifkhan@gmail.com>
2016-09-06 11:09:58 +0600

    Create 1253.cpp

---
+++ 1253-Misere-Nim/1253.cpp
@@ -0,0 +1,100 @@
+/** ========================================**
+ ** Bismillahi-Rahamanirahim.               **
+ ** @Author: A Asif Khan Chowdhury          **
+ ** ========================================**/
+
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <strings.h>
+#include <cmath>
+#include <cstdio>
+#include <set>
+#include <list>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+#define Set(N, j) (N|(1<<j))
+#define reset(N, j) (N&~(1<<j))
+#define Check(N, j) (bool)(N&(1<<j))
+#define toggle(N, j) (N^(1<<j))
+#define turnOff(N, j) (N & ~(1<<j))
+#define CLEAR(A, x) ( memset(A,x,sizeof(A)) )
+#define pii pair <int, int>
+#define pb push_back
+#define open freopen("D:/a.txt", "r", stdin);
+#define write freopen("D:/b.txt","w", stdout);
+#define inf (1ll<<28)
+#define ll long long
+#define ull unsigned ll
+#define mod 1000000007
+#define gc getchar
+#define ls(n) (n<<1)
+#define rs(n) ls(n)|1
+#define MID(a,b) ((a+b)>>1)
+#define fs first
+#define sc second
+#define mx 110
+
+template<class T>inline bool read(T &x) {
+    int c=getchar();
+    int sgn=1;
+    while((~c&&c<'0')||c>'9') {
+        if(c=='-')sgn=-1;
+        c=getchar();
+    }
+    for(x=0; ~c&&'0'<=c&&c<='9'; c=getchar())x=x*10+c-'0';
+    x*=sgn;
+    return ~c;
+}
+//int X[]= {-1, -1, -1, 0, 1, 1, 1, 0};   //x 8 direction
+//int Y[]= {-1, 0, +1, 1, 1, 0, -1, -1};  //y 8 direction
+int X[]= {-1, 0, 1, 0};   //x 4 direction
+int Y[]= { 0, 1, 0, -1};  //y 4 direction
+
+int n;
+int W[105];
+
+int main() {
+#ifdef DEBUG
+    freopen("/Users/abuasifkhan/Desktop/Programming/ProblemSolving/ProblemSolving/a.in", "r", stdin);
+    //    freopen("/Users/abuasifkhan/Desktop/Programming/ProblemSolving/ProblemSolving/b.out", "w", stdout);
+#endif
+    
+    int test;
+    read(test);
+    for(int C=1; C<=test; C++) {
+        printf("Case %d: ", C);
+        read(n);
+        ll x = 0ll;
+        int ones=0;
+        for(int i=0;i<n;i++){
+            read(W[i]);
+            if(W[i]==1)ones++;
+            x^=W[i];
+        }
+        if(ones==n){
+            if(ones%2==0){
+                puts("Alice");
+                continue;
+            }
+            else{
+                puts("Bob");
+                continue;
+            }
+        }
+        if(x==0)puts("Bob");
+        else puts("Alice");
+    }
+    
+    return 0;
+}
+
+
